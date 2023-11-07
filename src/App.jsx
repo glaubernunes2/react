0d6cba23cabab167e9690b76ba7cac3350cc64ae
glauber nunes <119985120+glaubernunes2@users.noreply.github.com>
2023-11-07 15:33:16 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import gp from './assets/gp.jpg'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Glauber</h1>
+      <img src={gp} alt="" />
+
+      <ul>
+        <li>idade : 26</li>
+        <li>linguagens : Python e Javascript</li>
+        
+      </ul>
+    </>
+  )
+}
+
+export default App
